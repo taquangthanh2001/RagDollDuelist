ce7117729a5589a8caac0a13f309fa3027c16eff
lmlstudio <lmlgamingstudio@gmail.com>
2021-09-06 10:34:01 +0700

    Updated Enemy AI
    Enemy will follow player

--- Assets/Scripts/ragdollArms.cs
+++ Assets/Scripts/ragdollArms.cs
@@ -15,14 +15,14 @@ public class ragdollArms : MonoBehaviour
         player = transform.parent.GetComponent<ragdollEnemyWalk>().target;
         if(transform.parent.GetComponent<ragdollEnemyWalk>().inRange)
         {
-            if(!rightArm && transform.parent.GetComponent<ragdollEnemyWalk>().onRight)
+            if(!rightArm && transform.parent.GetComponent<ragdollEnemyWalk>().onLeft)
             {
                 Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, 0);
                 Vector3 diff = playerPos - transform.position;
                 float rotationZ = Mathf.Atan2(diff.x, -diff.y) * Mathf.Rad2Deg;
                 rb.MoveRotation(Mathf.LerpAngle(rb.rotation, rotationZ, force * Time.fixedDeltaTime));
             }
-            if (rightArm && transform.parent.GetComponent<ragdollEnemyWalk>().onRight == false)
+            if (rightArm && transform.parent.GetComponent<ragdollEnemyWalk>().onLeft == false)
             {
                 Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, 0);
                 Vector3 diff = playerPos - transform.position;
--- Assets/Scripts/ragdollEnemyWalk.cs
+++ Assets/Scripts/ragdollEnemyWalk.cs
@@ -7,30 +7,28 @@ public class ragdollEnemyWalk : MonoBehaviour
     public GameObject target;
     public float dist;
     public GameObject hips;
-    public bool onRight;
+    public bool onLeft;
     public bool inRange;
     public Animator anim;
     public Rigidbody2D rb;
     public int enemySpeed = 6;
     public float distToPlayer;
-    
+
     void Update()
     {
         dist = Vector2.Distance(target.transform.position, hips.transform.position);
         if(target.transform.position.x < hips.transform.position.x)
         {
-            Debug.Log(target.transform.position.x);
-            Debug.Log(hips.transform.position.x);
-            onRight = true;
+           onLeft = true;
         }
         else
         {
-            onRight = false;
+            onLeft = false;
         }
         if(dist > 0)
         {
             inRange = false;
-            if(onRight)
+            if(onLeft)
             {
                 anim.Play("WalkBack");
                 rb.AddForce(Vector2.left * enemySpeed);
